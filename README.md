0cd9b6c6170ac06726424d5581106a8d539dbf3c
Sandeep Prasad <s3g007@gmail.com>
2014-12-29 01:06:39 +0530

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+REST In Peace
+=============
+
+Helps you in creating REST Services.
---
+++ index.js
@@ -0,0 +1,7 @@
+var RIP = {
+	version: "0.0.0",
+	name: "ripjs",
+	description: "REST In Peace. Helps you in creating REST Services."
+}
+
+module.exports = RIP;
